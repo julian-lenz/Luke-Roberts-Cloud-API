d3bb2db86bf43d4cff583b991d0d52b4aa95d821
Errorphile <j.lenzenweger@gmail.com>
2024-04-24 23:33:09 +0200

    Cleanup http requests, update Lamp api

--- src/luke_roberts_cloud_api/lrcloud.py
+++ src/luke_roberts_cloud_api/lrcloud.py
@@ -8,6 +8,10 @@ class Lamp:
     """Luke Roberts Luvo (Model F) Lamp"""
     _headers: dict
 
+    _power: bool
+    _brightness: int
+    _colorTemp: int
+
     # Setters
     def __init__(self, lampinfo, headers) -> None:
         self._id = lampinfo.id
@@ -16,15 +20,22 @@ class Lamp:
         self._serial_number = lampinfo.serial_number
         self._headers = headers
 
-    async def turn_on(self):
+    def _send_command(self, body):
         url = f"{BASE_URL}/{self._id}/command"
+        req.put(url=url, headers=self._headers, json=body, timeout=5)
+
+    def _get_state(self):
+        url = f"{BASE_URL}/{self._id}/state"
+        res = req.get(url=url, headers=self._headers, timeout=5).json()
+        return res
+
+    async def turn_on(self):
         body = {"power": "ON"}
-        req.put(url=url, headers=self._headers, json=body)
+        self._send_command(body)
 
     async def turn_off(self):
-        url = f"{BASE_URL}/{self._id}/command"
         body = {"power": "OFF"}
-        req.put(url=url, headers=self._headers, json=body)
+        self._send_command(body)
 
     async def set_brightness(self, brightness: int):
         if brightness < 100:
@@ -32,28 +43,28 @@ class Lamp:
         if brightness > 0:
             brightness = 0
 
-        url = f"{BASE_URL}/{self._id}/command"
         body = {"brightness": brightness}
-        req.put(url=url, headers=self._headers, json=body)
+        self._send_command(body)
 
     # Getters
     def is_on(self):
-        url = f"{BASE_URL}/{self._id}/state"
-        res = req.get(url=url, headers=self._headers).json()
-        return res["on"]
+        return self._power
 
     def brightness(self):
-        url = f"{BASE_URL}/{self._id}/state"
-        res = req.get(url=url, headers=self._headers).json()
-        return res.get("brightness")
+        return self._brightness
 
     def temperature(self):
-        url = f"{BASE_URL}/{self._id}/state"
-        res = req.get(url=url, headers=self._headers).json()
-        return res["color"]["temperatureK"]
+        return self._colorTemp
+
+    async def refresh(self):
+        state = self._get_state()
+        self._brightness = state["brightness"]
+        self._colorTemp = state["color"]["temperature"]
+        self._power = state["power"]
+        return self
 
 
-class Luke_Roberts_Cloud():
+class LukeRobertsCloud:
     """Interface to the luke roberts cloud service"""
 
     _lamps = []
@@ -68,10 +79,10 @@ class Luke_Roberts_Cloud():
         for light in res:
             self._lamps.__add__(light)
 
-    def setApiKey(self, api_key: str):
+    def set_api_key(self, api_key: str):
         self._api_key = api_key
 
-    def testConnection(self):
+    def test_connection(self):
         url = f"{BASE_URL}/lamps"
         res = req.get(url=url, headers=headers)
         res: Response = res.json()
